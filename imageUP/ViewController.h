19a73bd539eb385408092974dce855fbae06c4e1
sbishopwta <sbishop@alumni.berklee.edu>
2016-12-20 08:02:39 -0500

    Initial Commit

---
+++ imageUP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imageUP
+//
+//  Created by Steven Bishop on 12/20/16.
+//  Copyright © 2016 Steven Bishop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ imageUP/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  imageUP
+//
+//  Created by Steven Bishop on 12/20/16.
+//  Copyright © 2016 Steven Bishop. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
